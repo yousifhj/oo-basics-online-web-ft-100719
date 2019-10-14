52a8edce4a73d6f52ccc8edd7b20f7c496856760
Huda Yousif <hjdoski@gmail.com>
2019-10-14 22:30:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 
 class shoe
-  attr_accessor :color :size :material :condition 
+  attr_accessor :color, :size, :material, :condition 
   attr_reader :brand
   
   def initialize(brand)
