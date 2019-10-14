92e00083a7b0e08f9513315032459e6315e6a6e1
Huda Yousif <hjdoski@gmail.com>
2019-10-14 22:28:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 
 class shoe
-  attr_accessor :brand :color :size :material :condition 
-  attr_reader : 
+  attr_accessor :color :size :material :condition 
+  attr_reader :brand 
 end
